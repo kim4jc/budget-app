1b13e39a448371cdd62f1d156bf16ee069abf168
Nolan <nolan.brownsberger1@gmail.com>
2025-07-30 17:35:38 -0400

    Add Bin Database Implementation

--- server/models/Bins.js
+++ server/models/Bins.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     }, 
     userID: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true, // Should be false when authentication is implemented
         references: {
             model: 'Users', // Reference to the User model
             key: 'id', // Foreign key in the Bins table
